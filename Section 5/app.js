1af0f7b17d905fca75ec2cbc950e2ccea97f8264
Romain Neuville <neuvilleromain@gmail.com>
2020-02-29 14:45:28 +0100

    section 5 instance ok

--- Section 5/app.js
+++ Section 5/app.js
@@ -2,13 +2,22 @@ var data = {
     title: 'The VueJS Instance',
     showParagraph: false
   }
-  
+// Une instance sur un element (id), il faut donc passer par des composants avant l'initialisation des instances de vue
+// Ils seront accessible dans toutes les instances de vue
+// la propriété template est limité, une seule string
+Vue.component('hello', {
+  template: '<h1>Hello! Je suis un composant</h1>'
+});
+
   var vm1 = new Vue({
     data: data,
     methods: {
       show() {
         this.showParagraph = true;
         this.updateTitle('The VueJS Instance (Updated)');
+        // On peut accéder aux ref de notre DOM (propriété d'un élément: ref="") avec $refs, accessible en dehors de l'instance également !!
+        // Attention, cela modifie le DOM directement, et pas le template virtuel de Vue.js (rechargement : retour arriére possible)
+        // Interessant pour RECUPERER des informations ;)
         this.$refs.myButton.innerText = 'Test';
       },
       updateTitle(title) {
@@ -27,11 +36,13 @@ var data = {
     }
   });
   
+  // Pour monter une instance de vue sur un élément $el, ex on créé l'instance mais l'élément cible du template n'existe pas encore...
   vm1.$mount('#app1');
   // ------------------------------------------------
   // On ne peut pas ajouter de "data" aprés la création de l'instance, cela ne fonctionnera pas comme une data :
   vm1.newProp = "New!"
 
+  // la propriété vm1.$data correspond à l'objet data de l'isntance de vue, permet d'y accéder de l'extérieur
   console.log(vm1.$data === data);
   // vm1.$refs.heading.innerText = 'Something else';
   
@@ -45,20 +56,49 @@ var data = {
   var vm2 = new Vue({
     el: '#app2',
     data: {
-      title: 'The second Instance'
+      title: 'The second Instance, test lifecycle'
+    },
+    // <!-- Instance Lifecycle, on a des methodes permettant d'accéder aux différentes étapes de la vie de l'instance : -->
+    // <!-- beforeCreate() created() beforeMount() mounted() beforeUpdate() updated() beforeDestroy() -->
+    beforeCreate() {
+      console.log('beforeCreate()');
+    },
+    created() {
+      console.log('created()');
+    },
+    beforeMount() {
+      console.log('beforeMount()');
+    },
+    mounted() {
+      console.log('mounted()')
+    },
+    beforeUpdate() {
+      console.log('beforeUpdate()');
+    },
+    updated() {
+      console.log('updated()');
+    },
+    beforeDestroy() {
+      console.log('beforeDestroy()');
+    },
+    destroyed() {
+      console.log('destroyed()');
     },
     methods: {
       onChange: function() {
         // On accéde à la vm1 pour changer son titre :
         vm1.title = 'Changed!';
+      },
+      destroy(){
+        this.$destroy();
       }
     }
   });
   
+  // template, propriété pour spécifier le template dans l'instance
   var vm3 = new Vue({
       template: '<h1>Hello!</h1>'
   });
-  
+  // On peut monter l'élément avec du js vanilla
   vm3.$mount();
   document.getElementById('app3').appendChild(vm3.$el);
-  

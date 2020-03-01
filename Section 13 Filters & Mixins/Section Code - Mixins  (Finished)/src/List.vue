9f1c8fd9102fd80fd79d2b5542f47ad83e6730c3
Romain Neuville <neuvilleromain@gmail.com>
2020-03-01 17:33:06 +0100

    section 13

---
+++ Section 13 Filters & Mixins/Section Code - Mixins  (Finished)/src/List.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { fruitMixin } from './fruitMixin';
+
+    export default {
+        // Ces datas sont présentes dans le mixin avec le même nom, celle du composant actuel prévalent :
+        data() {
+            return {
+                fruits: ['Apple2', 'Banana2', 'Mango2', 'Melon2'],
+            }
+        },
+        mixins: [fruitMixin],
+        // Le composant a la priorité sur les mixins (executer aprés) en cas de conflit
+        created() {
+            console.log('Inside List Created Hook');
+        }
+    }
+</script>
+
+<style>
+</style>
